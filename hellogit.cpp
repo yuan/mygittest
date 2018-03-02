f076516dfe29edab2beb01ebf4a400e1fea728bd
yuan <943947609@qq.com>
2018-03-02 15:28:25 +0800

    create a hellogit cpp file

---
+++ hellogit.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main()
+{
+        printf("Hello world");
+            puts("Hello world");
+                cout << "Hello world" << endl;
+                    return 0;
+}
+
